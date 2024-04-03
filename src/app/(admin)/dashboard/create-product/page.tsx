852e35838996e5bffe0be884b174ac0fa72dce32
Yith Sopheaktra <yithsopheaktra18@gmail.com>
2024-04-04 00:45:22 +0700

    ADMIN : create product static image

--- src/app/(admin)/dashboard/create-product/page.tsx
+++ src/app/(admin)/dashboard/create-product/page.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
+import { ACCESS_TOKEN, BASE_URL } from "@/constants/constants";
+import axios from "axios";
 import {
 	Button,
 	Checkbox,
@@ -13,7 +15,14 @@ import {
 import React, { useState } from "react";
 
 export default function CreateProduct() {
+	const [productName, setProductName] = useState("");
+	const [productPrice, setProductPrice] = useState("");
+	const [productQuantity, setProductQuantity] = useState("");
+	const [productDescription, setProductDescription] = useState("");
 	const [imagePreview, setImagePreview] = useState<string | null>(null);
+    const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+
 	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const file = event.target.files?.[0];
 		if (file) {
@@ -22,11 +31,73 @@ export default function CreateProduct() {
 				setImagePreview(reader.result as string);
 			};
 			reader.readAsDataURL(file);
+			// Update the FormData object with the selected file
+
+			const formdata = new FormData();
+			formdata.append("name", "ISTAD Store Poster");
+			formdata.append("image", file, file.name);
 		}
 	};
+
+	const myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+	myHeaders.append(
+		"Authorization",
+		"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE0MzE5MzkzLCJpYXQiOjE3MTIxNTkzOTMsImp0aSI6IjRiOTQ5OTBiNWEyYzQwYWY4MTYzNjgyNjIwY2RkOGZjIiwidXNlcl9pZCI6NX0.O3xNaye1Q66woZdJoAlBVfaDMp9wJoXz7_GdBJaPMnw"
+	);
+	myHeaders.append(
+		"Cookie",
+		"csrftoken=UafcJBTYw7ngeP1Ov1FF91N7OXlKVb4Wv8keKsf7fMiqDLZZLD52Z5fr4NPp9X50; sessionid=8r4cx104gr3c4t6tasqsfzts7jvkcov9"
+	);
+
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		// // Ensure an image is selected
+		// if (!imageData) {
+		// 	alert("Please upload an image");
+		// 	return;
+		// }
+
+		try {
+
+			const formData = {
+				category: {
+					name: "running shoes",
+					icon: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1693342954-rincon-3-64ee5ca62e001.jpg?crop=1xw:1xh;center,top&resize=980:*",
+				},
+				name: productName,
+				desc: productDescription,
+				image: "imageUrl",
+				price: productPrice,
+				quantity: productQuantity,
+			};
+
+			// Send formData to your API endpoint
+			console.log(formData);
+			fetch(`${BASE_URL}products/`, {
+				method: "POST",
+				body: JSON.stringify(formData),
+				headers: myHeaders,
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					console.log("Success:", data);
+				})
+				.catch((error) => {
+					console.error("Error:", error);
+				});
+		} catch (error) {
+			console.error("Error uploading image:", error);
+		}
+	};
+
 	return (
 		<main className="grid place-content-center w-screen">
-			<form className="flex flex-col gap-4 border p-8 mt-10 w-[800px]">
+			<form
+				className="flex flex-col gap-4 border p-8 mt-10 w-[800px]"
+				onSubmit={handleSubmit}
+				encType="multipart/form-data"
+				method="POST">
 				<div>
 					<h2 className="text-center text-2xl">Create Product</h2>
 					<div className="mb-2 block mt-5">
@@ -37,6 +108,8 @@ export default function CreateProduct() {
 						type="text"
 						placeholder="Air Jordan 1"
 						required
+						value={productName}
+						onChange={(e) => setProductName(e.target.value)}
 					/>
 					<div className="mb-2 block mt-5">
 						<Label htmlFor="price" value="Product Price" />
@@ -46,6 +119,8 @@ export default function CreateProduct() {
 						type="text"
 						placeholder="$500"
 						required
+						value={productPrice}
+						onChange={(e) => setProductPrice(e.target.value)}
 					/>
 
 					<div className="mb-2 block mt-5">
@@ -56,10 +131,12 @@ export default function CreateProduct() {
 						type="text"
 						placeholder="20"
 						required
+						value={productQuantity}
+						onChange={(e) => setProductQuantity(e.target.value)}
 					/>
 				</div>
-                <div>
-                <div className="mb-2 block mt-5">
+				<div>
+					<div className="mb-2 block mt-5">
 						<Label
 							htmlFor="description"
 							value="Product Description"
@@ -69,11 +146,19 @@ export default function CreateProduct() {
 						className="h-[150px]"
 						id="description"
 						placeholder="Air Jordan 1 is a sneaker designed by Peter Moore, Michael Jordan's first signature shoe. It was created for the 1984-85 season and later banned by the NBA for breaking uniform regulations."
+						required
+						value={productDescription}
+						onChange={(e) => setProductDescription(e.target.value)}
 					/>
 					<div className="mb-2 block mt-5">
 						<Label htmlFor="file" value="Upload file" />
 					</div>
-					<FileInput id="file" helperText="Product Images" onChange={handleFileChange}/>
+					<FileInput
+						id="file"
+						helperText="Product Images"
+						onChange={handleFileChange}
+						name="file"
+					/>
 					{imagePreview && (
 						<div className="mt-5">
 							<img
@@ -83,9 +168,12 @@ export default function CreateProduct() {
 							/>
 						</div>
 					)}
-                </div>
-
-				<Button type="submit">Create New</Button>
+				</div>
+				<button
+					className="bg-blue-500 text-white p-3 rounded-xl"
+					type="submit">
+					Create New
+				</button>
 			</form>
 		</main>
 	);
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,4 @@
 export const BASE_URL = "https://store.istad.co/api/";
 export const IMAGE_PLACEHOLDER = "https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png";
-export const ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE0MzEyNDg1LCJpYXQiOjE3MTIxNTI0ODUsImp0aSI6IjM2NWIyNGVjNWU4NjQwNmNhNDkzYWY5YzU2MzI5YWU1IiwidXNlcl9pZCI6NX0.5S11ZS8IzCxsAZSNvMwWA4TCNJFayIM5DZ6xqfnszho"
+export const ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE0MzE5MzkzLCJpYXQiOjE3MTIxNTkzOTMsImp0aSI6IjRiOTQ5OTBiNWEyYzQwYWY4MTYzNjgyNjIwY2RkOGZjIiwidXNlcl9pZCI6NX0.O3xNaye1Q66woZdJoAlBVfaDMp9wJoXz7_GdBJaPMnw";
+
